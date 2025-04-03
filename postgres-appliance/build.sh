b61074f6f500ccdc10834644a3478892294fdaea
qeternity <ch@se.gd>
2025-04-03 17:13:31 +0100

    4.0-p5

--- postgres-appliance/build.sh
+++ postgres-appliance/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build --platform linux/amd64 -t zumainternal/spilo-17:4.0-p4 .
-docker tag zumainternal/spilo-17:4.0-p4 zumainternal/spilo-17:latest
+docker build --platform linux/amd64 -t zumainternal/spilo-17:4.0-p5 .
+docker tag zumainternal/spilo-17:4.0-p5 zumainternal/spilo-17:latest
 
-docker push zumainternal/spilo-17:4.0-p4
+docker push zumainternal/spilo-17:4.0-p5
 docker push zumainternal/spilo-17:latest
