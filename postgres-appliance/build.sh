fd94413dbcdad2b89ff3f9462439b17568971158
Enrique Shockwave <33002121+qeternity@users.noreply.github.com>
2026-06-28 20:13:02 +0100

    4.0-p9

--- postgres-appliance/build.sh
+++ postgres-appliance/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build --platform linux/amd64 -t zumainternal/spilo-17:4.0-p8 .
-docker tag zumainternal/spilo-17:4.0-p8 zumainternal/spilo-17:latest
+docker build --platform linux/amd64 -t zumainternal/spilo-17:4.0-p9 .
+docker tag zumainternal/spilo-17:4.0-p9 zumainternal/spilo-17:latest
 
-docker push zumainternal/spilo-17:4.0-p8
+docker push zumainternal/spilo-17:4.0-p9
 docker push zumainternal/spilo-17:latest
